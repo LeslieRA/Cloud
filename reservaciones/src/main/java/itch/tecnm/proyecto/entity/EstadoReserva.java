9c7b96d93a10f1ca4774f66a03832eacc825a2a5
LeslieRA <138533458+LeslieRA@users.noreply.github.com>
2025-12-05 09:38:14 -0600

    intento1

--- reservaciones/src/main/java/itch/tecnm/proyecto/entity/EstadoReserva.java
+++
@@ -1,9 +0,0 @@
-package itch.tecnm.proyecto.entity;
-
-public enum EstadoReserva {
-	PENDIENTE,
-	CONFIRMADO, // <-- NUEVO ESTADO
-    COMPLETADA, // Se llevó a cabo
-    CANCELADA
-
-}
--- reservaciones/src/main/java/itch/tecnm/proyecto/repository/AtenderRepository.java
+++
@@ -1,9 +0,0 @@
-package itch.tecnm.proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import itch.tecnm.proyecto.entity.Atender;
-
-public interface AtenderRepository extends JpaRepository<Atender, Integer>{
-
-}
